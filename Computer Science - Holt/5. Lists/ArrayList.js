34f2ea64a5ee47d5e4e9cbd4ed24fe04c179a66a
Boris Klco <klco@hotmail.sk>
2023-05-11 16:07:25 +0200

    arrList finsihed

--- Computer Science - Holt/5. Lists/ArrayList.js
+++ Computer Science - Holt/5. Lists/ArrayList.js
@@ -8,15 +8,12 @@ class ArrayList {
   push(value) {
     this.length++;
     this.data[this.length] = value;
-    console.log(value + " at index " + this.length);
     return value + " at index " + this.length;
     //
   }
 
   pop() {
-    console.log(this.length);
     this.length--;
-    console.log(this.data, this.length);
     //
   }
 
@@ -24,7 +21,6 @@ class ArrayList {
     if (index < 1 || index > this.length) {
       return "Out of Bounds";
     }
-    console.log(this.data[index]);
     return this.data[index];
     //
   }
@@ -37,71 +33,8 @@ class ArrayList {
     for (let x = index; x < this.length; x++) {
       this.data[x] = this.data[x + 1];
     }
-    console.log(this.data);
     return this.data;
   }
 }
 
 const newList = new ArrayList();
-//
-newList.push(1);
-newList.push(2);
-newList.push(3);
-newList.push(4);
-newList.push(5);
-newList.push(6);
-newList.push(7);
-newList.push(8);
-newList.push(9);
-newList.push(10);
-//
-newList.get(1);
-newList.get(2);
-newList.get(3);
-newList.get(4);
-newList.get(5);
-newList.get(6);
-newList.get(7);
-newList.get(8);
-newList.get(9);
-newList.get(10);
-//
-newList.delete(5);
-//
-newList.get(1);
-newList.get(2);
-newList.get(3);
-newList.get(4);
-newList.get(5);
-newList.get(6);
-newList.get(7);
-newList.get(8);
-newList.get(9);
-newList.get(10);
-//
-newList.push("New 5");
-//
-newList.get(1);
-newList.get(2);
-newList.get(3);
-newList.get(4);
-newList.get(5);
-newList.get(6);
-newList.get(7);
-newList.get(8);
-newList.get(9);
-newList.get(10);
-//
-newList.pop();
-//
-newList.get(1);
-newList.get(2);
-newList.get(3);
-newList.get(4);
-newList.get(5);
-newList.get(6);
-newList.get(7);
-newList.get(8);
-newList.get(9);
-newList.get(10);
-//
